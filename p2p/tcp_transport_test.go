8b2f41774a0471f4f52bc8fa92d3cf2034d18764
kotjuz <rkotarski10@gmail.com>
2025-08-10 19:42:49 +0200

    test: testing conn listening

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -9,6 +9,8 @@ import (
 func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
-
 	assert.Equal(t, tr.ListenAddress, listenAddr)
+
+	// Server
+	tr.ListenAndAccept()
 }
